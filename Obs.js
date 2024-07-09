1e910d9bef5df596e7e391644c329fbe118e004a
Mopaakk <guilhermecasimironds@gmail.com>
2024-07-08 21:48:56 -0300

    general

--- Obs.js
+++ Obs.js
@@ -6,4 +6,28 @@ function showText(chapterId) {
     const selectedText = document.getElementById(chapterId);
     selectedText.classList.add('active');
   }
-  
+
+  // Function to randomly play the GIF
+function playGif() {
+  const gifElement = document.getElementById('static-gif');
+  const staticSrc = gifElement.src;
+  const gifSrc = gifElement.getAttribute('data-gif');
+
+  // Set the static image first
+  gifElement.src = staticSrc;
+
+  // Chance to play the GIF every 5 seconds
+  setInterval(() => {
+      if (Math.random() > 0.4) { // 20% chance to play the GIF
+          gifElement.src = gifSrc;
+          setTimeout(() => {
+              gifElement.src = staticSrc;
+          }, 1500); // Play for 3 seconds
+      }
+  }, 5000);
+}
+
+// Initialize the GIF control
+document.addEventListener('DOMContentLoaded', () => {
+  playGif();
+});
