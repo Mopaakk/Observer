d190cb96c2d2534d0637bc05436ab131f5102361
Mopaakk <guilhermecasimironds@gmail.com>
2024-07-09 12:05:17 -0300

    Update Obs.js

--- Obs.js
+++ Obs.js
@@ -7,25 +7,21 @@ function showText(chapterId) {
     selectedText.classList.add('active');
   }
 
-  // Function to randomly play the GIF
-function playGif() {
-  const gifElement = document.getElementById('static-gif');
-  const staticSrc = gifElement.src;
-  const gifSrc = gifElement.getAttribute('data-gif');
-
-  // Set the static image first
-  gifElement.src = staticSrc;
-
-  // Chance to play the GIF every 5 seconds
-  setInterval(() => {
-      if (Math.random() > 0.2) { // 20% chance to play the GIF
-          gifElement.src = gifSrc;
-          setTimeout(() => {
-              gifElement.src = staticSrc;
-          }, 1500); // Play for 3 seconds
-      }
-  }, 10000);
-}
+  function playGif() {
+    const gifElement = document.getElementById('static-gif');
+    const staticSrc = gifElement.src;
+    const gifSrc = gifElement.getAttribute('data-gif');
+  
+    setInterval(() => {
+        if (Math.random() <= 0.05) { // 5% chance
+            gifElement.src = gifSrc;
+            setTimeout(() => {
+                gifElement.src = staticSrc;
+            }, 1500); 
+        }
+    }, 10000);
+  }
+  
 
 // Initialize the GIF control
 document.addEventListener('DOMContentLoaded', () => {
@@ -36,8 +32,8 @@ document.addEventListener('DOMContentLoaded', () => {
   function playRandomSong() {
       const song1 = document.getElementById('song1');
       const song2 = document.getElementById('song2');
-      const songs = [song1, song2].filter(song => song !== null); // Filter out null elements
-      if (songs.length === 0) return; // Exit if no songs are found
+      const songs = [song1, song2].filter(song => song !== null); 
+      if (songs.length === 0) return; 
 
       const randomIndex = Math.floor(Math.random() * songs.length);
       const chosenSong = songs[randomIndex];
@@ -45,6 +41,5 @@ document.addEventListener('DOMContentLoaded', () => {
       chosenSong.play();
   }
 
-  playRandomSong(); // Play a random song on page load
-});
+  playRandomSong(); 
 });
